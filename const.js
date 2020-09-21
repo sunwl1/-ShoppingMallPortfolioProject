b54dd854076e4f9d88972b883b8d9d081f8a2034
youngjae <dudwo0129@naver.com>
2020-09-21 17:42:49 +0900

    09.21
    09.21

---
+++ const.js
@@ -0,0 +1,13 @@
+const Pinterest = {
+
+    firstSrc : ['http://tcpschool.com/html-tag-attrs/link-hreflang','https://sciencelove.com/636','https://opentutorials.org/course/743/6548'],
+    
+
+
+     posArr : {
+        tops : ['100px','200px','300px'],
+        left : ['50px','80px','100px']
+        
+    },
+};
+
