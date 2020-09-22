cebca4bbed16a09321e3236ca0e98c6bae062b59
jwanu <70936487+jwanu@users.noreply.github.com>
2020-09-22 17:42:01 +0900

    메인페이지, 네비게이션 추가작업

--- js/main.js
+++ js/main.js
@@ -1,39 +1,68 @@
-const searchBox = document.querySelector('.searchFieldInput');
-const overlay = document.querySelector('.overlay');
-const searchFieldInput = document.querySelector('.searchFieldInput');
 const likesBtn = document.querySelector('.likesBtn');
 const cartBtn = document.querySelector('.cartBtn');
-const menuBtn = document.querySelector('.menuBtn');
-const menuCloseBtn = document.querySelector('.menuCloseBtn');
 
+const menuOn = () => {
+ document.querySelector('.overlay').style.display = 'block';
+ document.querySelector('.menu').classList.remove('menuHidden');
+ document.body.classList.add('lockScroll');
+};
+const menuOff = () => {
+ document.querySelector('.overlay').style.display = 'none';
+ document.querySelector('.menu').classList.add('menuHidden');
+ document.body.classList.remove('lockScroll');
+};
+const searchFocus = () => {
+ document.querySelector('.overlay').style.display = 'block';
+ document.querySelector('.searchFieldInput').style.border = '2px solid #111';
+ document.querySelector('.searchFieldInput').style.background = '#fff';
+ document.querySelector('.searchWrapper').classList.add('searchBoxFx');
+ document.querySelector('.dropdown').style.display = 'block';
+ document.body.classList.add('lockScroll');
+};
+const searchBlur = () => {
+ document.querySelector('.overlay').style.display = 'none';
+ document.querySelector('.searchFieldInput').style.border =
+  '2px solid transparent';
+ document.querySelector('.searchFieldInput').style.background = '#eee';
+ document.querySelector('.searchWrapper').classList.remove('searchBoxFx');
+ document.querySelector('.dropdown').style.display = 'none';
+};
 
-searchBox.addEventListener('focus',()=>{
-    overlay.style.display = 'block';
-    searchFieldInput.style.border = '2px solid #111';
-    searchFieldInput.style.background = '#fff';
-    document.querySelector('.searchWrapper').classList.add('searchBoxFx');
-    document.querySelector('.dropdown').style.display = 'block';
-    document.body.classList.add('lockScroll');
+document
+ .querySelector('.searchFieldInput')
+ .addEventListener('focus', () => searchFocus());
+
+document.querySelector('.menuBtn').addEventListener('click', () => menuOn());
+
+document
+ .querySelector('.menuCloseBtn')
+ .addEventListener('click', () => menuOff());
+
+document.querySelector('.overlay').addEventListener('click', () => {
+ searchBlur();
+ menuOff();
 });
 
-overlay.addEventListener('click',()=>{
-    overlay.style.display = 'none';
-    searchFieldInput.style.border = '2px solid transparent';
-    searchFieldInput.style.background = '#eee';
-    document.querySelector('.searchWrapper').classList.remove('searchBoxFx');
-    document.querySelector('.dropdown').style.display = 'none';
-    document.querySelector('.menu').classList.add('menuHidden');
-    document.body.classList.remove('lockScroll');
+document.querySelector('.allCategory').addEventListener('click', () => {
+ document.querySelector('.menuSubNav').classList.remove('menuSubHidden');
+ document.querySelector('.menuNav').classList.add('menuSubHidden');
+ document.querySelector('.menuBack').style.display = 'block';
+ document.querySelector('.menuBack').style.visibility = 'visible';
 });
 
-menuBtn.addEventListener('click',()=>{
-    overlay.style.display = 'block';
-    document.querySelector('.menu').classList.remove('menuHidden');
-    document.body.classList.add('lockScroll');
+document.querySelector('.menuBackBtn').addEventListener('click', () => {
+ document.querySelector('.menuSubNav').classList.add('menuSubHidden');
+ document.querySelector('.menuNav').classList.remove('menuSubHidden');
+ document.querySelector('.menuBack').style.display = 'none';
+ document.querySelector('.menuBack').style.visibility = 'hidden';
 });
 
-menuCloseBtn.addEventListener('click', ()=> {
-    overlay.style.display = 'none';
-    document.querySelector('.menu').classList.add('menuHidden');
-    document.body.classList.remove('lockScroll');
-})
+let arrAccor = document.querySelectorAll('.accordionBtn');
+for (let i = 0; i < arrAccor.length; i++) {
+    arrAccor[i].addEventListener('click', ()=>{
+        arrAccor[i].parentNode.nextElementSibling.classList.add('accoItemActive');
+        arrAccor[i].parentNode.parentNode.classList.add('itemActive');
+
+    })
+    
+}
