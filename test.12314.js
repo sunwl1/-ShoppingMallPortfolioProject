b47896e4044e25fedd18dfa49f54a20fdb30e44c
young jae Lee <dudwo0129@naver.com>
2020-09-26 17:53:51 +0900

    09.26
    circle hover

--- test.12314.js
+++ test.12314.js
@@ -1,124 +1,142 @@
 function showCircle(element, isVisible) {
-    const children = element.getElementsByClassName('parentCircle');
-    for (const i of children) {
-        i.querySelector('.chlidCircle').style.transition = null;
-        if (isVisible) i.style.visibility = 'visible';
-        else i.style.visibility = 'hidden';
-    }
+  const children = element.getElementsByClassName('parentCircle');
+  for (const i of children) {
+    i.querySelector('.chlidCircle').style.transition = null;
+    if (isVisible) i.style.visibility = 'visible';
+    else i.style.visibility = 'hidden';
+  }
 }
 
 function inTrnasCircle(element, isTrans) {
-    const circleChild = element.querySelector('.chlidCircle');
-    if (isTrans) {
-        element.style.width = '44px';
-        element.style.height = '44px';
-        element.style.border = '5px solid rgba(255,255,255,0.5)';
-    
-        circleChild.style.width = '15px';
-        circleChild.style.height = '15px';
-        circleChild.style.transition = '0.25s 0.25s ease-in-out';
-    } else {
-        element.style.width = '50px';
-        element.style.height = '50px';
-        element.style.border = 'none';
-    
-        circleChild.style.width = '20px';
-        circleChild.style.height = '20px';
-        circleChild.style.transition = '0.3s 0.3s ease-in-out';
-    }
+  const circleChild = element.querySelector('.chlidCircle');
+  if (isTrans) {
+    element.style.width = '44px';
+    element.style.height = '44px';
+    element.style.border = '5px solid rgba(255,255,255,0.5)';
+
+    circleChild.style.width = '15px';
+    circleChild.style.height = '15px';
+    circleChild.style.transition = '0.25s 0.25s ease-in-out';
+  } else {
+    element.style.width = '50px';
+    element.style.height = '50px';
+    element.style.border = 'none';
+
+    circleChild.style.width = '20px';
+    circleChild.style.height = '20px';
+    circleChild.style.transition = '0.3s 0.3s ease-in-out';
+  }
 }
 
-function Contaierbox(link, imgSrc){
-   
-    const linkBox = document.createElement('a');
-    const linkImg = document.createElement('img');
-    const linkDiv = document.createElement('div');
+function Contaierbox(link, imgSrc) {
+  const linkBox = document.createElement('a');
+  const linkImg = document.createElement('img');
+  const linkDiv = document.createElement('div');
 
-    linkBox.appendChild(linkImg);
-    linkDiv.appendChild(linkBox);
-    document.body.appendChild(linkDiv);
+  linkBox.appendChild(linkImg);
+  linkDiv.appendChild(linkBox);
+  document.body.appendChild(linkDiv);
 
-    linkBox.id = 'imgLinkBox';
-    linkBox.href = link;
-    linkBox.onmouseenter = () => { showCircle(linkBox, true); };
-    linkBox.onmouseleave = () => { showCircle(linkBox, false); };
+  linkBox.id = 'imgLinkBox';
+  linkBox.href = link;
+  linkBox.onmouseenter = () => {
+    showCircle(linkBox, true);
+  };
+  linkBox.onmouseleave = () => {
+    showCircle(linkBox, false);
+  };
 
-    linkDiv.style.top = '100px';
-    linkDiv.style.left = '100px';
-    linkDiv.style.width = '500px';
-    linkDiv.style.height = '500px';
-    linkDiv.style.position = 'relative';
+  linkDiv.style.top = '100px';
+  linkDiv.style.left = '100px';
+  linkDiv.style.width = '500px';
+  linkDiv.style.height = '500px';
+  linkDiv.style.position = 'relative';
 
-    linkImg.src = imgSrc;
-    linkImg.style.width = '100%';
-    linkImg.style.height = '100%';
+  linkImg.src = imgSrc;
+  linkImg.style.width = '100%';
+  linkImg.style.height = '100%';
 }
 
-Contaierbox('#','라이언 이미지.png')
+Contaierbox('#', '라이언 이미지.png');
 
 function createCir(arrPosition, element) {
-    arrPosition.forEach(e => {   //몰라 확실히
-        
-        const circle = document.createElement('div');
-        const circleChild = document.createElement('div');
-        element.appendChild(circle);
-        circle.appendChild(circleChild);
-        
-        circle.className = 'parentCircle';
-        circle.style.position = 'absolute';
-        circle.style.visibility = 'hidden';
-        circle.style.top = e[0] - (circle.offsetWidth / 2);
-        circle.style.left = e[1] - (circle.offsetWidth / 2);
-        circle.onmouseenter = () => { inTrnasCircle(circle, true); };
-        circle.onmouseleave = () => { inTrnasCircle(circle, false); };
+  arrPosition.forEach((e) => {
+    const circle = document.createElement('div');
+    const circleChild = document.createElement('div');
+    element.appendChild(circle);
+    circle.appendChild(circleChild);
 
-        circleChild.className = 'chlidCircle';
-    });
+    circle.className = 'parentCircle';
+    circle.style.position = 'absolute';
+    circle.style.visibility = 'hidden';
+    circle.style.top = circle.style.top = e[0] - circle.offsetWidth / 2;
+    circle.style.left = e[1] - circle.offsetWidth / 2;
+    circle.onmouseenter = () => {
+      inTrnasCircle(circle, true);
+    };
+    circle.onmouseleave = () => {
+      inTrnasCircle(circle, false);
+    };
 
+    circleChild.className = 'chlidCircle';
+  });
 }
 
-createCir([[100, 100], [300, 650], [100, 1200]], document.getElementById('imgLinkBox'));
+createCir(
+  [
+    [100, 100],
+    [300, 650],
+    [100, 1200],
+  ],
+  document.getElementById('imgLinkBox')
+);
 
 function createArrowBox(element, link, name, info, price) {
-    const arrowBoxLink = document.createElement('a');
-    const arrowBox = document.createElement('div');
-    const itemName = document.createElement('h2');
-    const itemInfo = document.createElement('p');
-    const itemPrice = document.createElement('p');
-    const arrowIcon = document.createElement('p');
+  const arrowBoxLink = document.createElement('a');
+  const arrowBox = document.createElement('div');
+  const itemName = document.createElement('h2');
+  const itemInfo = document.createElement('p');
+  const itemPrice = document.createElement('p');
+  const arrowIcon = document.createElement('p');
 
-    arrowBoxLink.appendChild(itemName);
-    arrowBoxLink.appendChild(itemInfo);
-    arrowBoxLink.appendChild(itemPrice);
-    arrowBoxLink.appendChild(arrowIcon);
-    arrowBox.appendChild(arrowBoxLink);
-    element.appendChild(arrowBox);
+  arrowBoxLink.appendChild(itemName);
+  arrowBoxLink.appendChild(itemInfo);
+  arrowBoxLink.appendChild(itemPrice);
+  arrowBoxLink.appendChild(arrowIcon);
+  arrowBox.appendChild(arrowBoxLink);
+  element.appendChild(arrowBox);
 
-    arrowBox.className = 'arrow_box';
-    arrowBox.style.position = 'absolute';
-    arrowBox.style.bottom = '70px';
-    arrowBox.style.width = '100px';
-    arrowBox.style.height = '80px';
-    arrowBox.style.left = '-27px';
+  arrowBox.className = 'arrow_box';
+  arrowBox.style.position = 'absolute';
+  arrowBox.style.bottom = '70px';
+  arrowBox.style.width = '100px';
+  arrowBox.style.height = '80px';
+  arrowBox.style.left = '-27px';
 
-    arrowBoxLink.href = link;
+  arrowBoxLink.href = link;
 
-    itemName.innerHTML = name;
-    itemName.style.width = '70px';
-    itemName.style.padding = '5px 5px 5px 4px';
+  itemName.innerHTML = name;
+  itemName.style.width = '70px';
+  itemName.style.padding = '5px 5px 5px 4px';
 
-    itemInfo.innerHTML = info;
-    itemInfo.style.width = '70px';
-    itemInfo.style.padding = '5px 5px 5px 4px';
+  itemInfo.innerHTML = info;
+  itemInfo.style.width = '70px';
+  itemInfo.style.padding = '5px 5px 5px 4px';
 
-    itemPrice.innerHTML = price;
-    itemPrice.style.width = '70px';
-    itemPrice.style.padding = '5px 5px 5px 4px';
+  itemPrice.innerHTML = price;
+  itemPrice.style.width = '70px';
+  itemPrice.style.padding = '5px 5px 5px 4px';
 
-    arrowIcon.style.position = 'absolute';
-    arrowIcon.style.left = '80px';
-    arrowIcon.style.top = '0px';
-    arrowIcon.innerHTML = '&gt;';
+  arrowIcon.style.position = 'absolute';
+  arrowIcon.style.left = '80px';
+  arrowIcon.style.top = '0px';
+  arrowIcon.innerHTML = '&gt;';
 }
 
-createArrowBox(document.querySelector('.parentCircle'), 'https://www.naver.com/', '가나다라', '마바사', '123123');
+createArrowBox(
+  document.querySelector('.parentCircle'),
+  'https://www.naver.com/',
+  '가나다라',
+  '마바사',
+  '123123'
+);
--- test.2.js
+++ test.2.js
@@ -1,137 +1,194 @@
-function showCircle(element, isVisible) {
-    const children = element.getElementsByClassName('parentCircle');
-    for (const i of children) {
-        i.querySelector('.chlidCircle').style.transition = 'none';
-        i.querySelector('.item_infoBox').style.transition = 'none';
-        if (isVisible) i.style.opacity = '1', i.style.visibility = 'visible',
-        i.style.transition = 'all 0.3s ease-in-out';
-        else i.style.opacity = '0',
-        i.style.transition = 'all 0.3s ease-in-out';
-    }
-}
+// function showCircle(element, isVisible) {
+//   let children = element.getElementsByClassName('parentCircle');
+//   for (const i of children) {
+//     i.querySelector('.chlidCircle').style.transition = 'none';
+//     i.querySelector('.item_infoBox').style.transition = 'none';
+//     if (isVisible)
+//       (i.style.opacity = '1'),
+//         (i.style.visibility = 'visible'),
+//         (i.style.transition = 'all 0.3s ease-in-out');
+//     else (i.style.opacity = '0'), (i.style.transition = 'all 0.3s ease-in-out');
+//   }
+// }
 
 function showItem(element, isVisible) {
-    const childItem = element.getElementsByClassName('parentCircle');
-    for (const i of childItem) {
-        if (isVisible) i.style.visibility = 'visible';
-        else i.style.visibility = 'hidden';
-    }
+  const childItem = element.getElementsByClassName('parentCircle');
+  for (const i of childItem) {
+    if (isVisible) i.style.visibility = 'visible';
+    else i.style.visibility = 'hidden';
+  }
 }
 
-function inTransCir(element, isTrans) {
-    const chlidCir = element.querySelector('.chlidCircle')
-    const chliditem = element.querySelector('.item_infoBox')
-    if(isTrans) {
-        element.style.border = '0.2rem solid rgba(255,255,255,0.5)';
-        chlidCir.style.width = '0.6rem';
-        chlidCir.style.height = '0.6rem';
-        chlidCir.style.transition = 'all 0.2s ease-in-out';
-        chliditem.style.visibility = 'visible';
-        chliditem.style.opacity = '1';
-        chliditem.style.transition = 'all 0.2s ease-in-out';
-    } else {
-        element.style.border = '0.2rem solid transparent';
-        chlidCir.style.width = '1rem';
-        chlidCir.style.height = '1rem';
-        chlidCir.style.transition = 'all 0.2s ease-in-out';
+// function inTransCir(element, isTrans) {
+//   const chlidCir = element.querySelector('.chlidCircle');
+//   const chliditem = element.querySelector('.item_infoBox');
+//   if (isTrans) {
+//     element.style.width = '1.2rem';
+//     element.style.height = '1.2rem';
+//     element.style.border = '0.1rem solid rgba(255,255,255,0.5)';
+//     chlidCir.style.width = '0.2rem';
+//     chlidCir.style.height = '0.2rem';
+//     chlidCir.style.transition = 'all 0.2s ease-in-out';
+//     chliditem.style.visibility = 'visible';
+//     chliditem.style.opacity = '1';
+//     chliditem.style.transition = 'all 0.2s ease-in-out';
 
-        chliditem.style.visibility = 'hidden'
-        chliditem.style.opacity = '0';
-        chliditem.style.transition = 'all 0.2s ease-in-out';
-    }
-}
+//     // if (matchMedia('min-wiedth: 56.25em').matches) {
+//     //   element.style.width = '2.5rem';
+//     //   element.style.height = '2.5rem';
+//     //   element.style.border = '0.3rem solid rgba(255,255,255,0.5)';
+//     //   chlidCir.style.width = '0.9rem';
+//     //   chlidCir.style.height = '0.9rem';
+//     //   chlidCir.style.transition = 'all 0.2s ease-in-out';
+//     //   chliditem.style.visibility = 'visible';
+//     //   chliditem.style.opacity = '1';
+//     //   chliditem.style.transition = 'all 0.2s ease-in-out';
+//     // }
+//     // if (matchMedia('min-width: 37.5em')) {
+//     //   element.style.width = '1.6rem';
+//     //   element.style.height = '1.6rem';
+//     //   element.style.border = '0.2rem solid rgba(255,255,255,0.5)';
+//     //   chlidCir.style.width = '0.6rem';
+//     //   chlidCir.style.height = '0.6rem';
+//     //   chlidCir.style.transition = 'all 0.2s ease-in-out';
+//     //   chliditem.style.visibility = 'visible';
+//     //   chliditem.style.opacity = '1';
+//     //   chliditem.style.transition = 'all 0.2s ease-in-out';
+//     // }
+//   } else {
+//     element.style.width = '1.2rem';
+//     element.style.height = '1.2rem';
+//     element.style.border = '0.1rem solid transparent';
+//     chlidCir.style.width = '0.5rem';
+//     chlidCir.style.height = '0.5rem';
+//     chlidCir.style.transition = 'all 0.2s ease-in-out';
+//     chliditem.style.visibility = 'hidden';
+//     chliditem.style.opacity = '0';
+//     chliditem.style.transition = 'all 0.2s ease-in-out';
 
+// if (matchMedia('min-width: 56.25em')) {
+//   element.style.width = '2.5rem';
+//   element.style.height = '2.5rem';
+//   element.style.border = '0.3rem solid transparent';
+//   chlidCir.style.width = '1.4rem';
+//   chlidCir.style.height = '1.4rem';
+//   chlidCir.style.transition = 'all 0.2s ease-in-out';
+//   chliditem.style.visibility = 'hidden';
+//   chliditem.style.opacity = '0';
+//   chliditem.style.transition = 'all 0.2s ease-in-out';
+// }
+// if (matchMedia('min-width: 37.5em')) {
+//   element.style.width = '1.6rem';
+//   element.style.height = '1.6rem';
+//   element.style.border = '0.2rem solid transparent';
+//   chlidCir.style.width = '0.8rem';
+//   chlidCir.style.height = '0.8rem';
+//   chlidCir.style.transition = 'all 0.2s ease-in-out';
+//   chliditem.style.visibility = 'hidden';
+//   chliditem.style.opacity = '0';
+//   chliditem.style.transition = 'all 0.2s ease-in-out';
+// }
+//   }
+// }
 const imgsContainer = document.createElement('div');
-    imgsContainer.id = 'container';
-    document.body.appendChild(imgsContainer);
-
-
+imgsContainer.id = 'container';
+document.body.appendChild(imgsContainer);
 
 function contaierBox(obj) {
- 
-    const linkBox = document.createElement('a');
-    const linkImg = document.createElement('img');
-    const linkDiv = document.createElement('div');
-    const linkSpan = document.createElement('span');
+  const linkBox = document.createElement('a');
+  const linkImg = document.createElement('img');
+  const linkDiv = document.createElement('div');
+  const linkSpan = document.createElement('span');
 
-    linkSpan.appendChild(linkImg)
-    linkBox.appendChild(linkSpan);
-    linkDiv.appendChild(linkBox);
-    imgsContainer.appendChild(linkDiv);
+  linkSpan.appendChild(linkImg);
+  linkBox.appendChild(linkSpan);
+  linkDiv.appendChild(linkBox);
+  imgsContainer.appendChild(linkDiv);
 
-    linkDiv.className = 'grid-item';
+  linkDiv.className = 'grid-item';
 
-    linkDiv.onmouseenter = () => { showCircle(linkDiv, true); };
-    linkDiv.onmouseleave = () => { showCircle(linkDiv, false); };
+  linkDiv.onmouseenter = () => {
+    showCircle(linkDiv, true);
+  };
+  linkDiv.onmouseleave = () => {
+    showCircle(linkDiv, false);
+  };
 
-    linkBox.id = 'imgLinkBox';
-    linkBox.href = obj.linkSrc;
-    linkBox.className = 'imgLinkBoxClass';
+  linkBox.id = 'imgLinkBox';
+  linkBox.href = obj.linkSrc;
+  linkBox.className = 'imgLinkBoxClass';
 
-    linkImg.src = obj.imgSrc;
-    linkImg.sizes = obj.imgSize;
-    linkImg.srcset = obj.imgSrcset;
-    linkImg.style.width = '100%';
-    linkImg.style.height = '100%';
-    linkImg.style.objectFit = 'cover';
+  linkImg.src = obj.imgSrc;
+  linkImg.sizes = obj.imgSize;
+  linkImg.srcset = obj.imgSrcset;
+  linkImg.style.width = '100%';
+  linkImg.style.height = '100%';
+  linkImg.style.objectFit = 'cover';
 
-    linkSpan.className = 'contaierWrap';
+  linkSpan.className = 'contaierWrap';
 
-    for (const i of obj.inner) {
-        const circlePosition = document.createElement('div');
-        const circleSrc = document.createElement('a');
-        const circle = document.createElement('div');
-        const circleChild = document.createElement('div');
-        circle.appendChild(circleChild);
-        circleSrc.appendChild(circle);
-        circlePosition.appendChild(circleSrc)
-        linkSpan.appendChild(circlePosition);
+  for (const i of obj.inner) {
+    const circlePosition = document.createElement('div');
+    const circleSrc = document.createElement('a');
+    const circle = document.createElement('div');
+    const circleChild = document.createElement('div');
+    circle.appendChild(circleChild);
+    circleSrc.appendChild(circle);
+    circlePosition.appendChild(circleSrc);
+    linkSpan.appendChild(circlePosition);
 
-        circleSrc.href = i.circle.src;
-        circle.id = 'idParentCircle';
-        circle.className = 'parentCircle';
-        circlePosition.style.position = 'absolute';
-        circlePosition.className = 'circle-position';
-        circlePosition.style.top = `${i.circle.top}%`;
-        circlePosition.style.left = `${i.circle.left}%`;
-        circle.onmouseover = () => { inTransCir(circle, true), showItem(circle, true); };
-        circle.onmouseout = () => { inTransCir(circle, false), showItem(circle, false); };
-    
-        circleChild.className = 'chlidCircle';
+    circleSrc.href = i.circle.src;
+    circle.id = 'idParentCircle';
+    circle.className = 'parentCircle';
 
-        const itemInfoBox = document.createElement('div');
-        const itemName = document.createElement('h2');
-        const itemInfo = document.createElement('p');
-        const itemPrice = document.createElement('p');
-        const arrowIcon = document.createElement('p');
+    circlePosition.style.position = 'absolute';
+    circlePosition.className = 'circle-position';
+    circlePosition.style.top = `calc(${i.circle.top}% - (${circle.offsetWidth}px / 2))`;
+    circlePosition.style.left = `calc(${i.circle.left}% - (${circle.offsetHeight}px / 2))`;
+    // circle.onmouseover = () => {
+    //   inTransCir(circle, true), showItem(circle, true);
+    // };
+    // circle.onmouseout = () => {
+    //   inTransCir(circle, false), showItem(circle, false);
+    // };
 
-        itemInfoBox.appendChild(itemName);
-        itemInfoBox.appendChild(itemInfo);
-        itemInfoBox.appendChild(itemPrice);
-        itemInfoBox.appendChild(arrowIcon);
-        circle.appendChild(itemInfoBox);
-    
-        itemInfoBox.className = 'item_infoBox';
-        itemInfoBox.onmouseover = () => { showItem(circle, true); };
-        itemInfoBox.onmouseout = () => { showItem(circle, false); };
-    
-        itemName.innerHTML = i.ItemBox.Name;
-        itemName.className = 'item-name';
-        itemName.id = i.ItemBox.Name;
-    
-        itemInfo.innerHTML = i.ItemBox.subInfo;
-        itemInfo.className = 'item-info';
-        itemInfo.id = i.ItemBox.subInfo;
-    
-   
-        itemPrice.innerHTML = `&bsol; ${i.ItemBox.price}`;
-        itemPrice.className = 'item-price';
-        itemPrice.id = i.ItemBox.price;
-    
-    
-        arrowIcon.className = 'arrow-icon';
-        arrowIcon.innerHTML = '&gt;';
+    circleChild.className = 'chlidCircle';
+
+    const itemInfoBox = document.createElement('div');
+    const itemName = document.createElement('h2');
+    const itemInfo = document.createElement('p');
+    const itemPrice = document.createElement('p');
+    const arrowIcon = document.createElement('p');
+
+    itemInfoBox.appendChild(itemName);
+    itemInfoBox.appendChild(itemInfo);
+    itemInfoBox.appendChild(itemPrice);
+    itemInfoBox.appendChild(arrowIcon);
+    circle.appendChild(itemInfoBox);
+
+    itemInfoBox.className = 'item_infoBox';
+    itemInfoBox.onmouseover = () => {
+      showItem(circle, true);
     };
+    itemInfoBox.onmouseout = () => {
+      showItem(circle, false);
+    };
+
+    itemName.innerHTML = i.ItemBox.Name;
+    itemName.className = 'item-name';
+    itemName.id = i.ItemBox.Name;
+
+    itemInfo.innerHTML = i.ItemBox.subInfo;
+    itemInfo.className = 'item-info';
+    itemInfo.id = i.ItemBox.subInfo;
+
+    itemPrice.innerHTML = `&bsol; ${i.ItemBox.price}`;
+    itemPrice.className = 'item-price';
+    itemPrice.id = i.ItemBox.price;
+
+    arrowIcon.className = 'arrow-icon';
+    arrowIcon.innerHTML = '&gt;';
+  }
 }
 
 contaierBox(imageState);
@@ -152,4 +209,75 @@ contaierBox(imageState14);
 contaierBox(imageState15);
 contaierBox(imageState16);
 
+// switch (matchMedia) {
+//     case 'min-width: 56.25em':
+//       element.style.width = '2.5rem';
+//       element.style.height = '2.5rem';
+//       element.style.border = '0.3rem solid rgba(255,255,255,0.5)';
+//       chlidCir.style.width = '0.9rem';
+//       chlidCir.style.height = '0.9rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'visible';
+//       chliditem.style.opacity = '1';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
+//     case 'min-width: 37.5em':
+//       element.style.width = '1.6rem';
+//       element.style.height = '1.6rem';
+//       element.style.border = '0.2rem solid rgba(255,255,255,0.5)';
+//       chlidCir.style.width = '0.6rem';
+//       chlidCir.style.height = '0.6rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'visible';
+//       chliditem.style.opacity = '1';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
 
+//     default:
+//       element.style.width = '1.2rem';
+//       element.style.height = '1.2rem';
+//       element.style.border = '0.1rem solid rgba(255,255,255,0.5)';
+//       chlidCir.style.width = '0.2rem';
+//       chlidCir.style.height = '0.2rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'visible';
+//       chliditem.style.opacity = '1';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
+//   }
+// } else {
+//   switch (matchMedia) {
+//     case 'min-width: 56.25em':
+//       element.style.width = '2.5rem';
+//       element.style.height = '2.5rem';
+//       element.style.border = '0.3rem solid transparent';
+//       chlidCir.style.width = '1.4rem';
+//       chlidCir.style.height = '1.4rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'hidden';
+//       chliditem.style.opacity = '0';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
+//     case 'min-width: 37.5em':
+//       element.style.width = '1.6rem';
+//       element.style.height = '1.6rem';
+//       element.style.border = '0.2rem solid transparent';
+//       chlidCir.style.width = '0.8rem';
+//       chlidCir.style.height = '0.8rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'hidden';
+//       chliditem.style.opacity = '0';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
+//     default:
+//       element.style.width = '1.2rem';
+//       element.style.height = '1.2rem';
+//       element.style.border = '0.1rem solid transparent';
+//       chlidCir.style.width = '0.5rem';
+//       chlidCir.style.height = '0.5rem';
+//       chlidCir.style.transition = 'all 0.2s ease-in-out';
+//       chliditem.style.visibility = 'hidden';
+//       chliditem.style.opacity = '0';
+//       chliditem.style.transition = 'all 0.2s ease-in-out';
+//       break;
+//   }
